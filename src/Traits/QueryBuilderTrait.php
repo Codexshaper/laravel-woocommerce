ad209dde2796652dc38c9aeab78663a53ed9af14
Md Abu Ahsan Basir <maab.career@gmail.com>
2019-09-27 15:59:01 +0600

    Updated find method

--- src/Traits/QueryBuilderTrait.php
+++ src/Traits/QueryBuilderTrait.php
@@ -41,8 +41,7 @@ trait QueryBuilderTrait
      */
     protected function find($id, $options = [])
     {
-        $result = collect(WooCommerce::find("{$this->endpoint}/{$id}", $options));
-        return $this;
+        return collect(WooCommerce::find("{$this->endpoint}/{$id}", $options));
     }
 
     /**
