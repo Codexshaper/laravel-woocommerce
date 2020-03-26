bbc60ce76e0fd411b4d2566f7fdcf7775e1d6d56
Md Abu Ahsan Basir <maab.career@gmail.com>
2020-03-27 05:28:09 +0600

    Replace ShippingZoneMethod instead of ShippingMethod

---
+++ src/Models/ShippingZoneMethod.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Codexshaper\WooCommerce\Models;
+
+use Codexshaper\WooCommerce\Facades\WooCommerce;
+
+class ShippingZoneMethod extends BaseModel
+{
+    protected $endpoint = 'shipping_methods';
+
+    /**
+     * Retrieve all Items
+     *
+     * @param array $options
+     *
+     * @return array
+     */
+    protected function all($options = [])
+    {
+        return WooCommerce::all($this->endpoint, $options);
+    }
+
+    /**
+     * Retrieve single Item
+     *
+     * @param integer $id
+     * @param array $options
+     *
+     * @return object
+     */
+    protected function find($id, $options = [])
+    {
+        return collect(WooCommerce::find("{$this->endpoint}/{$id}", $options));
+    }
+}
