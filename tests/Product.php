2fead987e1771c82aa0de1a679c945102dfb7ffd
Md Abu Ahsan Basir <maab.career@gmail.com>
2020-03-27 16:24:15 +0600

    Added phpunit test

---
+++ tests/Product.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CodexShaper\Dumper\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class Product extends TestCase
+{
+    /** @test */
+    public function it_provides_a_factory_method()
+    {
+        $this->assertSame(true, true);
+    }
+}
