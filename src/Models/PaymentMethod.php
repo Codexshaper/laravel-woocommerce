fd0fd11b6f56c57bd6457692122078a98efebfbf
CodexShaper <49456098+Codexshaper@users.noreply.github.com>
2020-10-17 14:28:16 +0600

    Update PaymentMethod.php

--- src/Models/PaymentMethod.php
+++ src/Models/PaymentMethod.php
@@ -2,34 +2,11 @@
 
 namespace Codexshaper\WooCommerce\Models;
 
-use Codexshaper\WooCommerce\Facades\WooCommerce;
+use Codexshaper\WooCommerce\Traits\QueryBuilderTrait;
 
 class PaymentMethod extends BaseModel
 {
+    use QueryBuilderTrait;
+   
     protected $endpoint = 'payment_gateways';
-
-    /**
-     * Retrieve all Items.
-     *
-     * @param array $options
-     *
-     * @return array
-     */
-    protected function all($options = [])
-    {
-        return WooCommerce::all($this->endpoint, $options);
-    }
-
-    /**
-     * Retrieve single Item.
-     *
-     * @param int   $id
-     * @param array $options
-     *
-     * @return object
-     */
-    protected function find($id, $options = [])
-    {
-        return WooCommerce::find("{$this->endpoint}/{$id}", $options);
-    }
 }
